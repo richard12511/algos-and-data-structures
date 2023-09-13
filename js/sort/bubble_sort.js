57f037ebc5ebad7a40bded7974d81b152b48162a
Richard Schmidt <richardnew12511@gmail.com>
2023-09-13 09:19:25 -0500

    proper impl

--- js/sort/bubble_sort.js
+++ js/sort/bubble_sort.js
@@ -1,14 +1,15 @@
-function bubbleSort(arr){
-    for (let i = 0; i < arr.length; i++){
-        for(let j = i + 1; j < arr.length; j++){
-            if(arr[i] > arr[j]){
-                const temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+
+function bubble_sort(arr) {
+    for(let i = 0; i < arr.length; ++i) {
+        for(let j = 0; j < arr.length - 1 - i; ++j) {
+            if(arr[j] > arr[j+1]) {
+                const temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
     }
     return arr;
 }
 
-module.exports = bubbleSort;
+module.exports = bubble_sort;
--- js/sort/bubble_sort.test.js
+++ js/sort/bubble_sort.test.js
@@ -1,18 +1,18 @@
-const bubbleSort = require('./bubble_sort');
+const bubble_sort = require('./bubble_sort');
 
 const arr1 = [4, 6, 3, 2, 9, 7];
 test('sort random array', () => {
-    expect(bubbleSort(arr1)).toEqual([2, 3, 4, 6, 7, 9])
+    const sortedArr = bubble_sort(arr1);
+    expect(sortedArr).toEqual([2, 3, 4, 6, 7, 9])
 })
-
 
 const arr2 = [4, 4, 4, 5];
 test('sort array with duplicate values', () => {
-    expect(bubbleSort(arr2)).toEqual([4, 4, 4, 5]);
+    expect(bubble_sort(arr2)).toEqual([4, 4, 4, 5]);
 })
 
 const arr3 = [9, 8, 7, 6];
 test('sort array in reverse order', () => {
-    const sortedArr = bubbleSort(arr3);
+    const sortedArr = bubble_sort(arr3);
     expect(sortedArr).toEqual([6, 7, 8, 9])
 })
